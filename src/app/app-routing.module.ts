809e5a854c8c8af98c26b5109bb8553b8572459f
Nicole Choi <clw19960@gmail.com>
2022-11-14 21:21:20 +0000

    display nav bar and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'recipes',
+        component: RecipesListComponent
+    },
+    {
+        path: 'recipes/:id',
+        component: RecipeComponent
+    },
+    {
+        path: 'new-recipe',
+        component: RecipeFormComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+const material = [
+    MatTableModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatPaginatorModule,
+    DragDropModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesListComponent,
+    RecipeComponent,
+    RecipeFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    CommonModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ...material
   ],
   providers: [],
   bootstrap: [AppComponent]
